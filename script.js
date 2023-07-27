3ff5723a337addb5f8f523a3c1fd8860ea77f709
Suhaib Abdirahman <139591958+Suhaib-Abdirahman@users.noreply.github.com>
2023-07-27 18:20:51 +0300

    Worked

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hello");
+var grade = 60;
+
+if (grade >= 90 && grade <= 100) {
+  console.log("You got an A grade");
+} else if (grade >= 75 && grade < 90) {
+  console.log("You got a B grade");
+} else if (grade >= 60 && grade < 70) {
+  console.log("You got a C grade");
+} else if(grade < 60){
+  console.log("You got a FAILED");
+}
